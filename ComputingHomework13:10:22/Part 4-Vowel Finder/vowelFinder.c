ce239ec866b361a7dc363d965d8bf0ab28332231
watergun127 <samuel@winslowstark.org>
2013-10-29 07:37:26 +0000

    Pushing for vowel homework

--- ComputingHomework13:10:22/Part 4-Vowel Finder/vowelFinder.c
+++ ComputingHomework13:10:22/Part 4-Vowel Finder/vowelFinder.c
@@ -1,6 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+/*char* deblank(char* input)                                         
+{
+    char *output=input;
+    for (int i = 0, j = 0; i<strlen(input); i++,j++)          
+    {
+        if (input[i]!=' ')                           
+            output[j]=input[i];                     
+        els
+            j--;                                     
+    }
+    output[j]=0;
+    return output;
+}*/
 int main()
 {
 	char stringToAnalyse[256];
@@ -12,7 +26,9 @@ int main()
 	int oNo;
 	int uNo;
 	int otherNo;
+	int spaceNo;
 	int i;
+	int j;
 	printf("Welcome to the vowel finder v1.0!\n");
 	while(1==1)
 	{
@@ -21,7 +37,9 @@ int main()
 		iNo=0;
 		oNo=0;
 		uNo=0;
-		printf("Please input a string to analyse\nString: ");
+		otherNo=0;
+		spaceNo=0;
+		printf("Please input a string to analyse, but without spaces please!\nString: ");
 		scanf("%s",stringToAnalyse);
 		//fprintf(stderr,"1");
 		strLength=strlen(stringToAnalyse);
@@ -49,16 +67,25 @@ int main()
 			{	
 				uNo++;
 			}
+			/*if(spaceNo>0)
+			{
+				for(j=i;stringToAnalyse[j]!='\0';j++) 
+				{ 
+					stringToAnalyse[j-1]=stringToAnalyse[j]; 
+				} 
+				//line[j]='\0'; 
+				spaceNo=0;
+			}*/
 		}
-		otherNo=strLength-(aNo+eNo+iNo+oNo+uNo);
-		printf("Number of a(s):%d\nNumber of e(s):%d\nNumber of i(s):%d\nNumber of o(s):%d\nNumber of u(s):%d\nNumber of other letter(s):%d\n",aNo,eNo,iNo,oNo,uNo,otherNo);
-		aNo*=(100/strLength);
-		eNo*=(100/strLength);
-		iNo*=(100/strLength);
-		oNo*=(100/strLength);
-		uNo*=(100/strLength);
-		otherNo*=(100/strLength);
-		printf("a %d%%\ne %d%%\ni %d%%\no %d%%\nu %d%%\nother %d%%\n",aNo,eNo,iNo,oNo,uNo,otherNo);
+			otherNo=strLength-(aNo+eNo+iNo+oNo+uNo);
+			printf("Number of a(s):%d\nNumber of e(s):%d\nNumber of i(s):%d\nNumber of o(s):%d\nNumber of u(s):%d\nNumber of other letter(s):%d\n",aNo,eNo,iNo,oNo,uNo,otherNo);
+			aNo*=(100/strLength);
+			eNo*=(100/strLength);
+			iNo*=(100/strLength);
+			oNo*=(100/strLength);
+			uNo*=(100/strLength);
+			otherNo*=(100/strLength);
+			printf("a %d%%\ne %d%%\ni %d%%\no %d%%\nu %d%%\nother %d%%\n",aNo,eNo,iNo,oNo,uNo,otherNo);
 	}
 	return 0;
 }
