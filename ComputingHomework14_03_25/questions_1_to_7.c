6facd5c671e1f133818eb8277773f1fffa5bea60
watergun127 <samuel@winslowstark.org>
2014-03-25 17:25:51 +0000

    New Folder

---
+++ ComputingHomework14_03_25/questions_1_to_7.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+int print_array(int array[3][3])
+{
+	int x,y;
+	for(x=0;x<3;x++){
+		for(y=0;y<3;y++){
+			printf("%d ",array[x][y]);
+			}
+		}
+	}
+	return 0;
+}
+int row_and_column(int array[3][3])
+{
+	int x,y;
+	printf("Row: ");
+	scanf("%d",&y);
+	printf("Column: ");
+	scanf("%d",&x);
+	return array[x][y];
+}
+int print_row(int array[3][3])
+{
+	int row,x;
+	printf("Row: ");
+	scanf("%d",&row);
+	for (x=0;x<3;x++){
+		printf("%d ",array[x][row]);
+	}
+	return 0;
+}
+int print_column(int array[3][3])
+{
+	int column,y;
+	printf("Column: ");
+	scanf("%d",&column);
+	for (y=0;y<3;y++){
+		printf("%d ",array[column][y]);
+	}
+	return 0;
+}
+int get_row_column(int array[3][3])
+{
+	int number=-1;
+	while (number>9||number<0){
+		printf("What number? ");
+		scanf("%d",&number);
+	}
+	int x,y;
+	for(x=0;x<3;x++){
+		for(y=0;y<3;y++){
+			if (array[x][y]==number){
+				printf("%d,%d",x,y);
+			}
+		}
+	}
+	return 0;
+}
+int is_in_list(int array[3][3], int target){
+	int x,y;
+	for(x=0;x<3;x++){
+		for(y=0;y<3;y++){
+			if (array[x][y]==target){
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+int main(){
+	srand(time(NULL));
+	int array[3][3] = [[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]];
+	int randNum=rand()%9,x,y;
+	for(x=0;x<3;x++){
+		for(y=0;y<3;y++){
+			while(is_in_list(array,randNum){
+				randNum=rand()%9;
+			}
+			array[x][y] = randNum;
+		}
+	}
+	return 0;
+}
