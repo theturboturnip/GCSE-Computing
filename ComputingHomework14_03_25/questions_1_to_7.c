448102ac92d3be0d246b37dabffa449d4c0afd30
watergun127 <samuel@winslowstark.org>
2014-04-01 09:02:59 +0100

    "AutomatedCommit01/04/2014"

--- ComputingHomework14_03_25/questions_1_to_7.c
+++ ComputingHomework14_03_25/questions_1_to_7.c
@@ -6,8 +6,8 @@ int print_array(int array[3][3])
 	for(x=0;x<3;x++){
 		for(y=0;y<3;y++){
 			printf("%d ",array[x][y]);
-			}
 		}
+        printf("\n");
 	}
 	return 0;
 }
@@ -18,26 +18,27 @@ int row_and_column(int array[3][3])
 	scanf("%d",&y);
 	printf("Column: ");
 	scanf("%d",&x);
-	return array[x][y];
-}
-int print_row(int array[3][3])
-{
-	int row,x;
-	printf("Row: ");
-	scanf("%d",&row);
-	for (x=0;x<3;x++){
-		printf("%d ",array[x][row]);
-	}
-	return 0;
+	printf("%d\n",array[x-1][y-1]);
 }
 int print_column(int array[3][3])
 {
-	int column,y;
+	int row,x;
 	printf("Column: ");
+	scanf("%d",&row);
+	for (x=0;x<3;x++){
+		printf("%d\n",array[x][row-1]);
+	}
+	return 0;
+}
+int print_row(int array[3][3])
+{
+	int column,y;
+	printf("Row: ");
 	scanf("%d",&column);
 	for (y=0;y<3;y++){
-		printf("%d ",array[column][y]);
+		printf("%d ",array[column-1][y]);
 	}
+    printf("\n");
 	return 0;
 }
 int get_row_column(int array[3][3])
@@ -70,15 +71,27 @@ int is_in_list(int array[3][3], int target){
 }
 int main(){
 	srand(time(NULL));
-	int array[3][3] = [[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]];
+	int array[3][3] = {{-1,-1,-1},{-1,-1,-1},{-1,-1,-1}};
 	int randNum=rand()%9,x,y;
 	for(x=0;x<3;x++){
 		for(y=0;y<3;y++){
-			while(is_in_list(array,randNum){
+			while(is_in_list(array,randNum)){
 				randNum=rand()%9;
 			}
 			array[x][y] = randNum;
+            //printf("%d",randNum);
 		}
 	}
+    printf("Print-Array\n");
+    print_array(array);
+    printf("From row and column\n");
+    row_and_column(array);
+    printf("Print column\n");
+    print_column(array);
+    printf("Print Row\n");
+    print_row(array);
+    printf("Get Address\n");
+    get_row_column(array);
+    printf("\n");
 	return 0;
 }
